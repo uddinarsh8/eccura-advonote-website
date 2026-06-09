094780dc0e0521ad1d58e1fb78aa8553aaa8f06a
uddinarsh8 <uddinarsh029@gmail.com>
2026-06-09 10:48:28 +0530

    Advonote full stack setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,93 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
-import Demo from "./pages/Demo";
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-
-        <Route
-          path="/"
-          element={<Home />}
-        />
-
-        <Route
-          path="/contact"
-          element={<Contact />}
-        />
-
-        <Route
-          path="/demo"
-          element={<Demo />}
-        />
-
-      </Routes>
-    </BrowserRouter>
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Advonote
+      </h1>
+    </div>
   );
 }
 
 export default App;
-import { Link } from "react-router-dom";
-
-function Navbar() {
-  return (
-    <nav>
-
-      <Link to="/">
-        Home
-      </Link>
-
-      <Link to="/contact">
-        Contact
-      </Link>
-
-      <Link to="/demo">
-        Demo
-      </Link>
-
-    </nav>
-  );
-}
-
-export default Navbar;
-function Hero() {
-
- return (
-
-  <section>
-
-   <h1>
-    Transform Legal Practice
-   </h1>
-
-   <p>
-    Smart Case Management
-   </p>
-
-  </section>
-
- );
-
-}
-
-export default Hero;
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-
-function Home(){
-
- return(
-
-  <>
-   <Navbar />
-   <Hero />
-  </>
-
- );
-
-}
-
-export default Home;
